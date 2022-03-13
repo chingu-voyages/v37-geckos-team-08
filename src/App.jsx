3cfc95256f181b50f8a296839773ee1bb80a0662
James Hall <jdhall@live.co.uk>
2022-03-13 10:09:37 +0800

    set up the routing to dashboard and started on dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+/* eslint-disable quotes */
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Jobs planner</h1>
       <h2>v37-geckos-team-08</h2>
+      {/* <Link to="/dashboard"> dashboards</Link> */}
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+/* eslint-disable comma-dangle */
+/* eslint-disable object-curly-newline */
+/* eslint-disable quotes */
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Dashboard from "./dashboard";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <Link to="/dashboard">dashboard</Link>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
